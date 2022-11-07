37293d7c49ea870702d60bc2a445b90667cd947e
acr0matic <work.acr0matic@gmail.com>
2022-11-07 18:47:52 +0300

    feat: "о нас говорят"

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,9 +1,33 @@
 
 if (isMobile) {
   const tour = new Swiper('.slider-tour', {
-    slidesPerView: 1.25,
+    slidesPerView: 1.1,
     speed: 300,
     spaceBetween: 16,
     autoHeight: true,
   });
 }
+
+const feedback = new Swiper('.slider-feedback', {
+  slidesPerView: 1.1,
+  speed: 300,
+  spaceBetween: 16,
+  autoHeight: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  pagination: {
+    el: '#feedback .swiper-pagination',
+    clickable: true,
+  },
+
+  breakpoints: {
+    600: {
+      slidesPerView: 1,
+      spaceBetween: 32,
+    },
+  }
+});
