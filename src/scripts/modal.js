9105035f97bf6cab358830ae3a7ce38c956f7fba
acr0matic <work.acr0matic@gmail.com>
2022-11-10 19:53:30 +0300

    feat: доработка модалок

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -7,6 +7,9 @@ if (tour) {
   if (modal) {
     const close = modal.querySelectorAll('.modal__close, .modal__swipe');
     const title = modal.querySelector('.modal__title');
+    const content = modal.querySelector('.modal__text');
+    const images = modal.querySelector('.gallery__slider .swiper-wrapper');
+    const additional = modal.querySelector('.modal__additional ');
 
     items.forEach(item => {
       item.addEventListener('click', () => {
@@ -15,6 +18,11 @@ if (tour) {
         }
 
         title.innerHTML = item.querySelector('.tour__title').innerHTML;
+        content.innerHTML = item.querySelector('.tour__content .content').innerHTML;
+        images.innerHTML = item.querySelector('.tour__content .gallery').innerHTML;
+        additional.innerHTML = item.querySelector('.tour__content .info').innerHTML;
+        lazyLoadInstance.update();
+
         gallery.slideTo(0);
 
         MicroModal.show(modal.id, modalParams);
